327b752319bee7cb1049809f5d833bbef924fed6
bjosef <33896160+bjosef@users.noreply.github.com>
2017-11-23 10:46:48 +0100

    Add files via upload
    Vehicle initialization function

---
+++ InitializeVehicles.m
@@ -0,0 +1,23 @@
+function vehicles = InitializeVehicles(numCars)
+%Code for initializing vechicle structure
+%Number cars in simulation should be given as input
+
+%Vehicles are identified by their index
+%1st field represents current node
+%2nd field represents current destination
+%3rd field represents final destination
+%4th field represents if vehicle is busy
+
+%Initializing random starting nodes of type 1
+names = {'Backaplan' 'Brunnsparken' 'Järntorget' 'Gamlestadstorget' 'Korsvägen'}';
+X = randi(length(names),1,numCars);
+
+%Initializing fields of vehicle structure
+field1 = 'CurrentNode';  value1 = names(X);
+field2 = 'CurrentDest';  value2 = '';
+field3 = 'FinalDest';  value3 = '';
+field4 = 'Busy';  value4 = false;
+
+vehicles = struct(field1,value1,field2,value2,field3,value3,field4,value4)
+
+end
