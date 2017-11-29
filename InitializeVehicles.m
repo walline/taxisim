f9ec0a7414227e82538afb8196ec1929b9536163
bjosef <33896160+bjosef@users.noreply.github.com>
2017-11-29 10:08:42 +0100

    Changed from strings to integers

--- InitializeVehicles.m
+++ InitializeVehicles.m
@@ -1,8 +1,8 @@
-function vehicles = InitializeVehicles(numCars,names)
+function vehicles = InitializeVehicles(numCars,positions)
 %Code for initializing vechicle structure
 %Number cars in simulation should be given as 1st input
 
-%List with names of suitable hubs to use as possible 
+%List with integers representing suitable hubs to use as possible 
 %start locations as 2nd input
 
 %Vehicles are identified by their index
@@ -11,14 +11,14 @@ function vehicles = InitializeVehicles(numCars,names)
 %3rd field represents final destination
 %4th field represents if vehicle is busy
 
-%Initializing random starting nodes of type 1
-X = randi(length(names),1,numCars);
+%Initializing random starting nodes from input list
+X = randi(length(positions),1,numCars);
 
 %Initializing fields of vehicle structure
-field1 = 'CurrentNode';  value1 = names(X);
+field1 = 'CurrentNode';  value1 = positions(X);
 %field2 = 'CurrentDest';  value2 = names(X);
-field2 = 'Path';  value2 = {{}};
-field3 = 'FinalDest';  value3 = names(X);
+field2 = 'Path';  value2 = [];
+field3 = 'FinalDest';  value3 = positions(X);
 field4 = 'Busy';  value4 = false;
 
 vehicles = struct(field1,value1,field2,value2,field3,value3,field4,value4)
