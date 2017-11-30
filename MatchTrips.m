aec3b0b51b423242589f4b0843874714cca64adf
bjosef <33896160+bjosef@users.noreply.github.com>
2017-11-30 17:15:14 +0100

    Update MatchTrips.m

--- MatchTrips.m
+++ MatchTrips.m
@@ -41,6 +41,7 @@ path = shortestpath(graph, cars(car0).CurrentNode, trips.tripMatrix(trip,1));
 cars(car0).Path = path;
 cars(car0).FinalDest = trips.tripMatrix(trip,2);
 cars(car0).Busy = 1;
+cars(car0).PairingTime = currentTime; 
 
 
 waitingTime = trips.PopTrip(id, currentTime);
