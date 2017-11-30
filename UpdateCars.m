3ab44d5398983725ba9b9cc832c27d83b0937968
bjosef <33896160+bjosef@users.noreply.github.com>
2017-11-30 17:14:22 +0100

    Update UpdateCars.m

--- UpdateCars.m
+++ UpdateCars.m
@@ -1,51 +1,44 @@
-function [cars, trips, timesArray] = MatchTrips(cars, trips, graph, currentTime, timesArray, parameter)
+function updatedCars = UpdateCars( graph, cars, currentTime, timesArray )
+%UPDATECARS
 
-%takes a selection parameter [0, 1] to decide how to assign trips;
-%0 ignores the distance, 1 ignores the queue
-
-%trips.tripMatrix =
-%[origin,destination,callTime,id]
-
-queueSize = size(trips.tripMatrix, 1);
-
-if(queueSize == 0)
-   return
-end
-
-tripOrigin = trips.tripMatrix(1,1);
-id =  trips.tripMatrix(1,4);
-trip = 1;
-[firstTripDistance, car0] = FindClosestCar(cars, tripOrigin, graph);
-
-if(car0 == 0)
-    return
-end
-
-bestDistance = firstTripDistance*parameter;
-
-for i = 2:queueSize
-    tripOrigin = trips.tripMatrix(i,1);
-    [tripDistance, car] = FindClosestCar(cars, tripOrigin, graph);
-    if tripDistance < bestDistance
-        id = trips.tripMatrix(i,4);
-        trip = i;
-        car0 = car;
-        bestDistance = tripDistance;
+numberOfCars = length(cars);
+for i=1:numberOfCars
+    car = cars(i);
+    if car.Busy
+        weight = graph.Edges.Weight(findedge(graph, car.CurrentNode, car.Path(find(car.CurrentNode)+1)));
+        if isempty(car.LastNodeTime)
+            elapsedTime = currentTime - car.PairingTime;
+        else
+            elapsedTime = currentTime - car.LastNodeTime;
+        end
+        if (weight - elapsedTime) == 0
+            car.CurrentNode = car.Path(2);
+            car.Path = car.Path(2:end);
+            car.LastNodeTime = currentTime;
+            if length(car.Path) == 1 % Car reached end of path
+                if car.CurrentNode == car.FinalDest % Trip complete
+                    car.FinalDest = [];
+                    car.Path = [];
+                    car.LastNodeTime = [];
+                    car.Busy = 0;
+                    totalTripTime = currentTime - car.PairingTime;
+                    timesArray(2, car.TimesArrayPosition) = totalTripTime;                 
+                else % Car reached passenger, find path to final dest
+                    [path, ~] = shortestpath(graph, car.CurrentNode, car.FinalDest);
+                    car.Path = path;
+                end
+            end  
+        end
+    elseif car.CurrentNode ~= car.FinalDest % Car on its way to hub
+        weight = graph.Edges.Weight(findedge(graph, car.CurrentNode, car.FinalDest));
+        elapsedTime = currentTime - car.LastNodeTime; 
+        if (weight - elapsedTime) == 0
+            car.CurrentNode = car.FinalDest;
+            car.FinalDest = [];
+            car.LastNodeTime = [];
+        end
     end
+    cars(i) = car;
 end
-
-path = shortestpath(graph, cars(car0).CurrentNode, trips.tripMatrix(trip,1));
-
-
-
-cars(car0).Path = path;
-cars(car0).FinalDest = trips.tripMatrix(trip,2);
-cars(car0).Busy = 1;
-cars(car0).PairingTime = currentTime; 
-
-
-waitingTime = trips.PopTrip(id, currentTime);
-timesArray = [timesArray, [waitingTime;0]];
-timesArrayPosition = size(timesArray, 2);
-cars(car0).TimesArrayPosition = timesArrayPosition;
+updatedCars = cars;
 end
