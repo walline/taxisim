0d25e5510feffe571e7a663ffc1179758be3f94d
walline <erik.i.wallin@gmail.com>
2017-11-30 11:02:59 +0100

    ddd

--- TripQueue.m
+++ TripQueue.m
@@ -1,21 +1,36 @@
-classdef TripQueue
+classdef TripQueue < handle
     properties
         tripMatrix
+        delayTime
     end
     
     methods
-        function obj = TripQueue()
+        function obj = TripQueue(delayTime)
             obj.tripMatrix = [];
+            obj.delayTime = delayTime;
         end
         
-        function AddTrip(origin,destination,callTime,id)
-            tripMatrix = [tripMatrix; [origin,destination,callTime,id]];
+        function AddTrip(obj,origin,destination,callTime,id)
+            obj.tripMatrix = [obj.tripMatrix; [origin,destination,callTime,id]];
         end
         
-        function waitingTime = PopTrip(id,pickUpTime)
-            index = find(tripMatrix(:,3)==id)
-            waitingTime = pickUpTime - callTime;
-            tripMatrix[index,:] = [];
+        function waitingTime = PopTrip(obj,id,pickUpTime)
+            index = find(obj.tripMatrix(:,4)==id);
+
+            if isempty(index)
+                disp('ERROR: Trip ID not found')
+                return
+            end
+            
+            callTime = obj.tripMatrix(index,3);
+            
+            if callTime > pickUpTime
+                disp('ERROR: Pick up time small than call time')
+                return
+            end
+            
+            waitingTime = max(pickUpTime - (obj.delayTime + callTime),0);
+            obj.tripMatrix(index,:) = [];
         end
         
         
