f0838af684e7b7dec7852c05690bfd7776b6d421
bjosef <33896160+bjosef@users.noreply.github.com>
2017-12-02 20:51:33 +0100

    Added plot of number of busy cars

--- RunSimulation.m
+++ RunSimulation.m
@@ -1,11 +1,14 @@
 %Initialization
+
+
 clc, clf, clear
 %Parameters
-numCars = 5; 
+numCars = 6;
 numHubs = 5;
 endTime = 1440; %1440 for 24 hours
 delayTime = 15;
 startTime = 0;
+counter_busy = zeros(1,endTime);
 
 totalNumberOfTrips = 10;
 functionCalls = 100; % change this depending on nr of loop iterations
@@ -35,7 +38,7 @@ tripQueue = TripQueue(delayTime);
 %Creating graph
 G = InitializeGraph();
 
-%Retrieving positions of the most connected nodes 
+%Retrieving positions of the most connected nodes
 D = degree(G);
 [B,positions] = maxk(D,numHubs);
 positions = num2cell(positions);
@@ -46,20 +49,41 @@ vec = InitializeVehicles(numCars,positions);
 %Main loop
 
 for t=1:endTime
- DisplayGraph(G, vec, tripQueue)
- pause(0.0001)
+    subplot(1,2,1)
+    DisplayGraph(G, vec, tripQueue)
+    
+    
+    pause(0.0001)
     %Update generate and add trip
-      [origin,destination] = probGen.GenerateTrip(t);
-
+    [origin,destination] = probGen.GenerateTrip(t);
+    
     if ((origin ~=0) && (destination ~=0) )
         tripQueue.AddTrip(origin,destination,t,ID_trip);
         ID_trip = ID_trip + 1;
     end
-
+    
     %Pair cars with trip
     [vec, tripQueue, timesArray] = MatchTrips(vec,tripQueue,G,t,timesArray, selection_para);
-
+    
     %Uppdate cars
-     [vec,  timesArray] = UpdateCars(G,vec,t,timesArray);
+    [vec,  timesArray] = UpdateCars(G,vec,t,timesArray);
+    timesArray
+    t
+    
+    
+    for i=1:numCars
+        if vec(i).Busy==1
+            counter_busy(t) = counter_busy(t) + 1;
+        end
+    end
+    
+    
+    subplot(1,2,2)
+    axis([0 endTime 0 numCars])
+    scatter(t, counter_busy(t),'s')
+     hold on
+    
+    title('#BusyCars')
+    
+    
 end
-
