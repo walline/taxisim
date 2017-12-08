115a1bd9ac84ba5d2797a96d03684bf579a1d513
Adam Liberda <liberda.a@gmail.com>
2017-12-08 13:28:58 +0100

    Updated cost func

--- CalculateCost.m
+++ CalculateCost.m
@@ -1,8 +1,8 @@
 function cost = CalculateCost( timesArray, numCars )
 %CALCULATECOST Calculate cost given waiting times and numbers of cars
 
-avgHourlySalary = 30000/175;
-carCost = 300000;
+avgHourlySalary = 25000/175;
+carCost = 300000/(5*365); % Car cost per day assuming 5yrs life length
 waitingTimes = sum(timesArray(1:2,:)); % Sum time to trip match with time to pickup
 scaledWaitingTimes = waitingTimes((waitingTimes > 15)); 
 scaledWaitingTimes = sum(scaledWaitingTimes)/60; % Hours
