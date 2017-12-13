b458f4b81bf1f45ed58f8f60370157957a0e136a
Adam Liberda <liberda.a@gmail.com>
2017-12-13 18:54:21 +0100

    Updated cost func, run-file for cost calc.

--- CalculateCost.m
+++ CalculateCost.m
@@ -1,13 +1,14 @@
 function cost = CalculateCost( timesArray, numCars, notPairedTime )
 %CALCULATECOST Calculate cost given waiting times and numbers of cars
 
-avgHourlySalary = 25000/175;
-carCost = 300000/(5*365); % Car cost per day assuming 5yrs life length
+avgHourlySalary = 22750/175; %Median salary gothenburg
+carCost = 400000/(2*365); % Car cost per day assuming 2yrs life length
+fuelCost = (6570/365) * 0.7 * 14; %(mil/day) * (l/mil) * 14 (kr/l)  
 waitingTimes = sum(timesArray(1:2,:)); % Sum time to trip match with time to pickup
 scaledWaitingTimes = waitingTimes((waitingTimes > 15)); 
 scaledWaitingTimes = (sum(scaledWaitingTimes) + notPairedTime)/60; % Hours
 
-cost = scaledWaitingTimes*avgHourlySalary + numCars*carCost;
+cost = scaledWaitingTimes*avgHourlySalary + numCars*(carCost+fuelCost);
 
 end
 
---
+++ RunCostSimulation.m
@@ -0,0 +1,138 @@
+%Initialization
+
+
+clc, clf, clear all, close all
+%Parameters
+numCars = linspace(1, 50, 50);
+numHubs = 5;
+endTime = 1440; %1440 for 24 hours
+delayTime = 15;
+startTime = 0;
+counter_busy = zeros(1,endTime);
+
+totalNumberOfTrips = 70;
+functionCalls = 1440; % change this depending on nr of loop iterations
+
+% numberOfPeople = totalNumberOfTrips/3.5; % very rough approximation
+
+load('fitdata.mat') % loads data for curve fits
+load('typedata.mat') % loads data for trip types
+
+%Creating graph
+[G, X, Y] = InitializeGraph();
+G_old = G;
+[G, X, Y] = AddEmptyNodes(G, 2, X, Y);
+h = InitializePlot(G, X, Y);
+
+iterations = 2;
+
+for ppl=1:3
+    if ppl == 1
+        totalNumberOfTrips = 70;
+    elseif ppl == 2
+        totalNumberOfTrips = 123;
+    else
+        totalNumberOfTrips = 175;
+    end
+    
+    costs = zeros(1, length(numCars));
+    for iteration=1:iterations
+        disp('Ppl')
+        disp(ppl)
+        disp('iteration')
+        disp(iteration)
+        % set last input true if you want to generate plots
+        probGen = ProbabilityGenerator();
+        probGen.SetTimeProbabilities(x,y,totalNumberOfTrips,functionCalls,startTime,endTime,false);
+        probGen.SetTypeProbabilities(G,homeDest,homeOrigin,workDest,workOrigin,xType,false);
+        
+        
+        selection_para = 0;
+        %Selection parameter [0, 1] to decide how to assign trips;
+        %0 ignores the distance, 1 ignores the queue
+        
+        for carAmount=1:length(numCars)
+            carNbr = numCars(carAmount);
+            disp(carNbr)
+            %Variables
+            ID_trip = 1; %Id for each trip
+            timesArray =  []; %row 1 pairing time row 2 trip length
+            
+            
+            %Initializing TripQueue
+            tripQueue = TripQueue(delayTime);
+            
+            
+            %Retrieving positions of the most connected nodes
+            D = degree(G);
+            positions = zeros(1, numHubs);
+            for i=1:numHubs
+                [~,I] = max(D);
+                D(I) = 0;
+                positions(i) = I;
+            end
+            positions = num2cell(positions);
+            
+            %Initializing vehicles
+            vec = InitializeVehicles(carNbr,positions);
+            
+            %Main loop
+          
+            
+            for t=1:endTime
+
+                numBusy = 0;
+                for i=1:carNbr
+                    if vec(i).Busy==1
+                        numBusy = numBusy + 1;
+                    end
+                end
+                
+                %Update generate and add trip
+                [origin,destination] = probGen.GenerateTrip(t);
+                
+                if ((origin ~=0) && (destination ~=0) )
+                    tripQueue.AddTrip(origin,destination,t,ID_trip);
+                    ID_trip = ID_trip + 1;
+                end
+                
+                %Pair cars with trip
+                [vec, tripQueue, timesArray] = MatchTrips(vec,tripQueue,G,t,timesArray, selection_para);
+                
+                vec = IdleCar(vec, G_old, 4, t);
+                
+                
+                %Uppdate cars
+                [vec,  timesArray] = UpdateCars(G,vec,t,timesArray);
+            
+
+            end
+            timeNotPaired = sum(endTime - tripQueue.tripMatrix(:, 3));
+            costs(carNbr) = costs(carNbr) + CalculateCost(timesArray, carNbr, timeNotPaired);
+        end
+    end
+    disp(costs)
+    costs = costs ./ 10;
+    if ppl == 1
+        save('costs_20ppl_2iter', 'costs')
+    elseif ppl == 2
+        save('costs_35ppl_2iter', 'costs')
+    else
+        save('costs_50ppl_2iter', 'costs')
+    end 
+end
+
+%%
+
+numCars = linspace(1, 50, 50);
+fifty_ppl = load('costs_50ppl.mat');
+thirty_ppl = load('costs_35ppl.mat');
+twenty_ppl = load('costs_20ppl.mat');
+figure()
+plot(numCars, twenty_ppl.costs, 'r')
+hold on
+plot(numCars, thirty_ppl.costs, 'b')
+plot(numCars, fifty_ppl.costs, 'g')
+legend('20 ppl', '35 ppl', '50 ppl')
+xlabel('Number of cars')
+ylabel('Cost')
